0f2ea02e2ee8cc20d800209b176fd181e6b5ed98
pheei <phe@gwu.edu>
2016-11-26 23:45:47 -0500

    Update the output data from JSON plain format to Spring JSON view format

--- src/main/java/com/moneytracker/controller/Controllers.java
+++ src/main/java/com/moneytracker/controller/Controllers.java
@@ -1,24 +1,24 @@
 package com.moneytracker.controller;
 
 import com.moneytracker.core.CoreFunctions;
-import com.moneytracker.model.PredictOutput;
-import com.moneytracker.model.SpendAndIncome;
+import com.moneytracker.model.Result;
+import com.moneytracker.model.ResultWithoutCreditPayment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
-
-@RestController
+@Controller
 public class Controllers {
 
     @Autowired
     private CoreFunctions coreFunctions;
 
     @RequestMapping(value={"/transaction/{type}"}, method= RequestMethod.GET)
-    public Object getTransactions(@PathVariable("type") String type,
-                                  @RequestParam(value = "no-donut", defaultValue = "false") boolean nodonut,
-                                  @RequestParam(value = "no-credit", defaultValue = "false") boolean nocredit){
+    public String getTransactions(Model model, @PathVariable("type") String type,
+                                  @RequestParam(value = "no_donut", defaultValue = "false") boolean nodonut,
+                                  @RequestParam(value = "no_credit", defaultValue = "false") boolean nocredit){
 
         //load all transactions from the GetAllTransactions endpoint
         coreFunctions.getAllTransactionsList();
@@ -35,15 +35,21 @@ public class Controllers {
 
         //remove transactions that has credit card payment
         if (nocredit){
-            PredictOutput output = new PredictOutput();
+            ResultWithoutCreditPayment output = new ResultWithoutCreditPayment();
             output.setSet(coreFunctions.removeCreditTransaction());
             output.setMap(coreFunctions.generateOutput());
-            return output;
+            model.addAttribute(output);
+            return "jsonTemplate";
         }
 
 
         //return the data model
-        return coreFunctions.generateOutput();
+        Result result = new Result();
+        result.setMap(coreFunctions.generateOutput());
+        model.addAttribute(result);
+
+        return "jsonTemplate";
+
 
     }
 
--- src/main/java/com/moneytracker/model/Result.java
+++ src/main/java/com/moneytracker/model/Result.java
@@ -3,18 +3,13 @@ package com.moneytracker.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by hpishepei on 11/26/16.
  */
-public class PredictOutput {
+public class Result {
     @JsonProperty("All_Transactions")
     private Map<String, SpendAndIncome> map;
-
-    @JsonProperty("Removed_Transactions")
-    private Set<Transaction> set;
-
 
     public Map<String, SpendAndIncome> getMap() {
         return map;
@@ -22,13 +17,5 @@ public class PredictOutput {
 
     public void setMap(Map<String, SpendAndIncome> map) {
         this.map = map;
-    }
-
-    public Set<Transaction> getSet() {
-        return set;
-    }
-
-    public void setSet(Set<Transaction> set) {
-        this.set = set;
     }
 }
